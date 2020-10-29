56fc5703facbdf6e3f5e103e7ca5627a5f005295
Nik Dawolev <gamesoft@abv.bg>
2020-10-29 18:20:38 +0000

    DateTime class measure time between two periods

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+include 'DateTime.php';
+
+
+print_r(time().'<br>');
+print_r(date('dd/mm/yy').'<br>');
+$dt = new DateTimes();
+print_r(microtime().'<br>');
+
+print_r(DateTimes::showTime().'<br>');
+
+
+
+
+
+
+
